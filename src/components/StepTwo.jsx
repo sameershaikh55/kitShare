7e841e4764ed3b2c11edee635ab1744e83e29645
sameer-shaikh <sameermubeen4@gmail.com>
2021-05-27 03:11:13 +0500

    normal added

--- src/components/StepTwo.jsx
+++ src/components/StepTwo.jsx
@@ -3,11 +3,26 @@ import { TextField } from "@material-ui/core";
 import error from "../assets/error.png";
 
 const StepTwo = ({ inpData, handleChange }) => {
+	const [state, setState] = React.useState("");
+	console.log(state);
+	const productUrl = state.split("/");
+	const dpIndex = productUrl.indexOf("dp");
+	console.log(productUrl);
+	const assin = `http://images.amazon.com/images/P/${
+		productUrl[dpIndex + 1]
+	}.01._SCTZZZZZZZ_.jpg`;
 	return (
 		<div className="stepTwo">
 			<h2>Upload an Image</h2>
 			<br />
 			<TextField
+				name="productInp"
+				value={state}
+				onChange={(e) => setState(e.target.value)}
+				className="inp"
+				label="Enter Product Link"
+			/>
+			{/* <TextField
 				name="productInp"
 				value={inpData.productInp}
 				onChange={handleChange}
@@ -22,11 +37,11 @@ const StepTwo = ({ inpData, handleChange }) => {
 				onChange={handleChange}
 				className="inp"
 				label="Enter Image Link"
-			/>
+			/> */}
 			<br />
 			<br />
 			<br />
-			<div className="imgContainer">
+			{/* <div className="imgContainer">
 				{(inpData.imageInp && (
 					<a href={inpData.productInp} target="blank">
 						<>
@@ -34,6 +49,9 @@ const StepTwo = ({ inpData, handleChange }) => {
 						</>
 					</a>
 				)) || <img src={error} alt="" />}
+			</div> */}
+			<div className="imgContainer">
+				<img src={assin} alt="Product Not Found" />
 			</div>
 		</div>
 	);
